dcc025d83e9bbb9548d0ef282b5506b3b48bbc75
Pytholearn <109828387+Pytholearn@users.noreply.github.com>
2025-08-14 20:46:04 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+import time
+import autoupgrader
+
+
+autoupgrader.set_url("https://raw.githubusercontent.com/Pytholearn/Proxy-Checker/refs/heads/main/version")
+autoupgrader.set_download_link("https://github.com/Pytholearn/Proxy-Checker.git")
+autoupgrader.set_current_version("1.6.3")
+
+if not autoupgrader.is_up_to_date():
+    print("Would you like to update your tool?")
+    choice = input("Enter (y/n): ")
+    if choice == "y":
+       autoupgrader.update()
+    else:
+        pass
+
+            
+with open("http_proxies.txt", "r") as file:
+    proxies_list = [line.strip() for line in file if line.strip()]
+
+error_list = 0
+good_list = 0
+
+
+ip_check_url = "http://api.ipify.org"  
+with open("good_proxies.txt", "w") as good_file:
+    
+    for index, proxy in enumerate(proxies_list, start=1):
+        proxies = {"http": proxy, "https": proxy}
+        
+        try:
+            
+            response = requests.get(ip_check_url, proxies=proxies, timeout=10)
+            
+            
+            if response.status_code == 200:
+                good_list += 1
+                print(f"{index}/{len(proxies_list)} ✅ Proxy: {proxy} - IP: {response.text} - Good: {good_list} - Bad: {error_list}")
+                good_file.write(proxy + "\n")  
+            else:
+                error_list += 1
+                print(f"{index}/{len(proxies_list)} ❌ Proxy: {proxy} - Status: {response.status_code} - Good: {good_list} - Bad: {error_list}")
+        
+        
+        except requests.exceptions.Timeout:
+            error_list += 1
+            print(f"{index}/{len(proxies_list)} ⏱️ Timeout Error: {proxy} - Good: {good_list} - Bad: {error_list}")
+        except requests.exceptions.ProxyError:
+            error_list += 1
+            print(f"{index}/{len(proxies_list)} 🔌 Proxy Error: {proxy} - Good: {good_list} - Bad: {error_list}")
+        except requests.exceptions.RequestException as e:
+            error_list += 1
+            print(f"{index}/{len(proxies_list)} ⚠️ Other Error: {proxy} - {str(e)} - Good: {good_list} - Bad: {error_list}")
+        
+        
+        time.sleep(2)
+
+
+print(f"\n🔍 Good Proxy: {good_list} - Bad Proxy: {error_list}")
